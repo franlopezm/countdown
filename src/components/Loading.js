0cfdc80eb62c4caeddf2ca2f13c9fab92c89bc4a
Francisco López Muñoz <frans.lopez.m@gmail.com>
2020-01-06 19:26:46 +0100

    Added loading

---
+++ src/components/Loading.js
@@ -0,0 +1,23 @@
+import './loading.scss'
+import React from 'react'
+
+
+export default ({ isLoading }) => {
+
+	return (
+		<div className="loading">
+			<div className="loading-grid">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+	)
+}
