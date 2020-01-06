42ae9392a5485c3b4644833d5b4cbfa0267cdb0a
Francisco López Muñoz <frans.lopez.m@gmail.com>
2020-01-06 19:39:07 +0100

    Updates

---
+++ precache-manifest.3d3f128120f178eb37a5cea81bd6818b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7114cb0f6d97e97b1eeb2b54a18e5444",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "2204f403361db1598137",
+    "url": "/countdown/static/css/main.90de6cab.chunk.css"
+  },
+  {
+    "revision": "1dfbc50f4d959275956c",
+    "url": "/countdown/static/js/2.b339cafe.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/countdown/static/js/2.b339cafe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2204f403361db1598137",
+    "url": "/countdown/static/js/main.e9e1d724.chunk.js"
+  },
+  {
+    "revision": "55297e517649b6e143e5",
+    "url": "/countdown/static/js/runtime-main.082449fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/countdown/precache-manifest.cb7f4bd955d941da3cd7f6e83d630b14.js"
+  "/countdown/precache-manifest.3d3f128120f178eb37a5cea81bd6818b.js"
 );
 
 self.addEventListener('message', (event) => {
