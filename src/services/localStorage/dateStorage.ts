f8e9d241aac7d124c026d792cd144c1934a01c29
franlopezm <frans.lopez.m@gmail.com>
2025-01-17 11:45:53 +0300

    fix: dateStorage change exports for export default

--- src/services/localStorage/dateStorage.ts
+++ src/services/localStorage/dateStorage.ts
@@ -3,14 +3,14 @@ import { DATE_STORAGE_KEY } from "../../config/constants"
 interface DateStorageItem {
   date: string
   isSince: boolean // When the value is true the date is in the past.
-  timezone?: string
+  timezone: string
 }
 
 const save = (data: DateStorageItem[]): void => {
   localStorage.setItem(DATE_STORAGE_KEY, JSON.stringify(data))
 }
 
-export const findAll = (): DateStorageItem[] => {
+const findAll = (): DateStorageItem[] => {
   const rawData = localStorage.getItem(DATE_STORAGE_KEY)
   if (!rawData) return []
 
@@ -22,7 +22,7 @@ export const findAll = (): DateStorageItem[] => {
   }
 }
 
-export const insertOne = (item: DateStorageItem): void => {
+const insertOne = (item: DateStorageItem): void => {
   const { date, isSince, timezone = '' } = item
 
   const list = findAll()
@@ -40,8 +40,14 @@ export const insertOne = (item: DateStorageItem): void => {
   }
 }
 
-export const removeOne = (position: number): void => {
+const removeOne = (position: number): void => {
   const list = findAll()
   list.splice(position, 1)
   save(list)
+}
+
+export default {
+  removeOne,
+  insertOne,
+  findAll
 }
