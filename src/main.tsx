f440e8e9ce4064339ae4dc2eefda1677267388c1
franlopezm <frans.lopez.m@gmail.com>
2025-01-18 15:57:33 +0300

    feat: Add RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import App from './components/App.tsx'
+import App from './App.tsx'
+import { RouterProvider } from './Router/context/RouterProvider.tsx'
 import { DateProvider } from './contexts/DateContext/DateProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DateProvider>
-      <App />
+      <RouterProvider>
+        <App />
+      </RouterProvider>
     </DateProvider>
   </StrictMode>,
 )
