0a126358deac693c7c708b194effc60796c7bcf3
franlopezm <frans.lopez.m@gmail.com>
2025-01-23 15:28:04 +0300

    style: Update Navbar styles

--- src/Navbar/NavbarItem.tsx
+++ src/Navbar/NavbarItem.tsx
@@ -14,15 +14,14 @@ const NavbarItem = (props: NavbarItemProps) => {
   const className = isActive ? 'bg-slate-200' : ''
 
   return (
-    <RouteLink to={to}>
-      <div
-        className={`text-xl mr-1 px-2 cursor-pointer flex items-center rounded hover:bg-slate-200 ${className}`}
-      >
-        {
-          icon && icon
-        }
-        {title}
-      </div>
+    <RouteLink
+      className={`text-xl mr-2 px-2.5 py-1 flex items-center rounded hover:bg-slate-200 ${className}`}
+      to={to}
+    >
+      {
+        icon && icon
+      }
+      {title}
     </RouteLink>
   )
 }
