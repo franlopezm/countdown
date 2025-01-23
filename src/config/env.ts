2967f032b4d4bef85b5408a7a0162a6d9cf07e69
franlopezm <frans.lopez.m@gmail.com>
2025-01-23 16:53:41 +0300

    feat: Add demo mode and example data

---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+const { VITE_LOAD_DEMO } = import.meta.env
+
+export const LOAD_DEMO = VITE_LOAD_DEMO === 'true'
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import { LOAD_DEMO } from './config/env'
+import { insertDemoData } from './demo'
 import { RouterProvider } from './Router'
 import { DateProvider } from './DateContainer'
 import App from './App'
+
+if (LOAD_DEMO) {
+  insertDemoData()
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
