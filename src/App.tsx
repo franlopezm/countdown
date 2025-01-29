1e6649bc111c53847ae9abb7fd38d25f077adeba
franlopezm <frans.lopez.m@gmail.com>
2025-01-29 16:22:24 +0100

    fix: App responsive

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className='bg-white w-full h-screen'>
       <Navbar />
 
-      <div className='px-10 md:px-24 xl:px-36 2xl:px-52 pb-10 pt-12'>
+      <div className='px-4 md:px-24 xl:px-36 2xl:px-52 pb-5 pt-5 md:pb-10 md:pt-12'>
         <RouteSwitch
           notFoundText='Lo sentimos, la página a la que desea acceder no existe.'
         >
